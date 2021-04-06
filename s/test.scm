ec9ba3329bd8dd1649ded4bd2d409c37a8edeab6
Moonchild <elronnd@elronnd.net>
2021-04-06 16:34:42 -0700

    utf8 enc/dec

---
+++ s/test.scm
@@ -0,0 +1,15 @@
+(defmacro assert (x)
+  `(unless ,x
+     (error 'assertion-failure ,(format #f "assertion ~a failed" x))))
+
+;(format #t "have ~a~%" (macroexpand (assert #t)))
+(let ((unitab '((#'a 97 "a")
+                (#'á 225 "á")
+                (#'⍝ 9053 "⍝")
+                (#'𐀏 65551 "𐀏"))))
+  (map (lambda ($)
+         (assert (= (car $) (cadr $)))
+         (assert (string=? (utf8-encode (car $)) (caddr $))))
+       unitab))
+(assert (equal? '(#q/ab\c/#q/r\/ #q{ab cd {de }f } #q//)
+                '("ab\\c" "r\\" "ab cd {de }f " "")))
